483c098174980db129523c6474f0f870e6ae2e70
Fiza Aggarwal <fiza.aggarwal@cars24.com>
2025-03-21 14:47:59 +0530

    basic test layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Dashboard from "./pages/Dashboard";
-import Quiz from "./pages/Quiz";
-import Simulation from "./pages/Simulation";
-import NotFound from "./pages/NotFound";
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import Dashboard from './pages/Dashboard';
+import Quiz from './pages/Quiz';
+import Simulation from './pages/Simulation';
+import NotFound from './pages/NotFound';
+import Test from './pages/test';
 import { ProctoringSystem } from "./components/Proctoring";
 import { ProctorTestScreen } from "./components/ProctoringTest";
 // import Proctor from "./pages/proctor";
@@ -19,16 +19,17 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner position="bottom-right" />
+      <Sonner position='bottom-right' />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/simulation" element={<Simulation />} />
+          <Route path='/' element={<Index />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/quiz' element={<Quiz />} />
+          <Route path='/simulation' element={<Simulation />} />
           <Route path="/proctor" element={<ProctorTestScreen />} />
+          <Route path='/test' element={<Test />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
